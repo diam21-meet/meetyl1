6adeb86942645c1a91a5ab8cd46a34d048a2521f
diam21-meet <52971399+diam21-meet@users.noreply.github.com>
2019-11-10 13:00:40 -0500

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -8,21 +8,23 @@ class User():
 	def add_friend(self,email):
 		self.email=email
 		self.friends_list.append(email)
-		print(self.name" has added "self.email" as a friend")
+		print(self.name+" has added "+self.email+" as a friend")
 	def remove_friend(self,email):
 		self.email=email
 		self.friends_list.remove(email)
-		print(self.name" has removed "self.email" as a friend")
+		print(self.name+" has removed "+self.email+" as a friend")
 	def post(self,text):
-		self.text= input()
-		print(self.name" has posted: "self.text)
+		self.text = text
+		self.text.append(text)
+		print(self.name+" has posted: "+self.text)
 	def get_userInfo():
 		print("___________")
-		print("name: "self.name)
-		print("password: "self.password)
-		print("freinds: "self.friends_list)
-		print("post: "self.post)
+		print("name: "+self.name)
+		print("password: "+self.password)
+		print("freinds: "+self.friends_list)
+		print("post: "+self.gethu.compost)
 		print("___________")
 Jack=User("Jack", "jack.blah@email.com","Myhiddenpassword123")
 Sam=User("Sam","sam.bbbbb@email.com","games")
 Jack.add_friend("sam.bbbbb@email.com")
+Sam.post()
