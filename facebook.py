da0c213e56a178da0df60d053890822d2dcbadc0
diam21-meet <52971399+diam21-meet@users.noreply.github.com>
2019-11-17 11:45:27 -0500

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -4,27 +4,37 @@ class User():
 		self.email=email
 		self.password=password
 		self.friends_list=[]
-		self.post=[]
+		self.posts=[]
 	def add_friend(self,email):
 		self.email=email
 		self.friends_list.append(email)
-		print(self.name+" has added "+self.email+" as a friend")
+		print(self.name + " has added " + self.email + " as a friend")
 	def remove_friend(self,email):
 		self.email=email
 		self.friends_list.remove(email)
-		print(self.name+" has removed "+self.email+" as a friend")
+		print(self.name + " has removed " + self.email + " as a friend")
 	def post(self,text):
-		self.text = text
-		self.text.append(text)
-		print(self.name+" has posted: "+self.text)
-	def get_userInfo():
-		print("___________")
-		print("name: "+self.name)
-		print("password: "+self.password)
-		print("freinds: "+self.friends_list)
-		print("post: "+self.gethu.compost)
-		print("___________")
+		self.posts.append(text)
+		print(self.name + "has posted " + text)
+	def getuser_info(self):
+		print("---------")
+		print(self.name)
+		print(self.email)
+		print(self.password)
+		print(self.friends_list)
+		print(self.posts)
 Jack=User("Jack", "jack.blah@email.com","Myhiddenpassword123")
 Sam=User("Sam","sam.bbbbb@email.com","games")
 Jack.add_friend("sam.bbbbb@email.com")
-Sam.post()
+Sam.post('hello')
+Jack.getuser_info()
+Sam.getuser_info()
+class Post():
+	def __init__(self,name,email,text):
+		self.name=name
+		self.email=email
+		self.text=text
+		self.post_list=[]
+	def post(self):
+		self.posts_list.append(text)
+		print(self.name + " has posted " + text)
